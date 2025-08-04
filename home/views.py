2d27488773d2ef2dad885d182b58d8bb9dbdd34f
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-04 15:00:30 +0530

    19765

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
+from requests
 from django.shortcuts import render
+
+
+def homepage(request):
+    response=requests.get('http://localhost:8000/api/menu-items/')
+    menu_items=[]
+
+    if response.status_code=200:
+        menu_items=response.json()
+    return render(request,'menu/home.html',{'menu_items':menu_items})
+
 
 # Create your views here.
