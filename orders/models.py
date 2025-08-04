115abf5bb06bf17ffd21218df9ca08d1176a1bb1
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-04 14:51:47 +0530

    19764

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    phone_number=models.CharField(max_length=15)
+
+    def __str__(slef):
+        return slef.name
 
 # Create your models here.
