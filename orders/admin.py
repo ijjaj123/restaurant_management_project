e50fd302df490a53fa59ea549e31e591cee4a562
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-05 10:15:39 +0530

    19767

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile,Menu,Order
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Menu)
+admin.site.register(Order)
