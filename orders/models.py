ddb582dc22468889f07fafcc6b41f6cee46c8c6e
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-05 10:07:27 +0530

    19766

--- orders/models.py
+++ orders/models.py
@@ -11,3 +11,17 @@ class UserProfile(models.Model):
         return slef.name
 
 # Create your models here.
+class Order(models.Model):
+    ORDER_STATUS_CHOICES=[
+        ('PENDING','pending'),
+        ('COMPLETED','completed'),
+        ('CANCELLED','cancelled'),
+    ]
+    customer=models.FroeignKey(User,on_delete=models.CASCADE,related_name='orders')
+    items=models.ManyToManyField(Menu,related_name='orders')
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
+    status=models.CharField(max_length=10,choices=ORDER_STATUS_CHOICES,default='PENDING')
+    ordered_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(slef):
+        return f"Order #{self.id} by{self.customer.username}"
