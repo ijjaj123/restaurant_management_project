6fd3c9025bae66af4665ac43eb93965144cc4319
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-05 10:11:13 +0530

    19767

--- orders/models.py
+++ orders/models.py
@@ -7,7 +7,7 @@ class UserProfile(models.Model):
     email=models.EmailField(unique=True)
     phone_number=models.CharField(max_length=15)
 
-    def __str__(slef):
+    def __str__(self):
         return slef.name
 
 # Create your models here.
@@ -23,5 +23,5 @@ class Order(models.Model):
     status=models.CharField(max_length=10,choices=ORDER_STATUS_CHOICES,default='PENDING')
     ordered_at=models.DateTimeField(auto_now_add=True)
 
-    def __str__(slef):
+    def __str__(self):
         return f"Order #{self.id} by{self.customer.username}"
