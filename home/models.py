1a1e653c39c5e3df92f2599977e6093ce2be49ff
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-06 14:33:19 +0530

    21833

--- home/models.py
+++ home/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(Max_length=100)
+    email=models.EmailField()
 
+    def __str__(self):
+        return self.name
