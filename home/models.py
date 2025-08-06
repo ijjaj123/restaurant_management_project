6e8de4699e0f3b16e0369506dc92413651705767
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-06 15:23:31 +0530

    Auto-save home/models.py

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name=models.CharField(Max_length=100)
+    name=models.CharField(max_length=100)
     email=models.EmailField()
 
     def __str__(self):
