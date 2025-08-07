575f5e3d5d3206fd47c13069495d61cf71aa8537
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-07 14:38:58 +0530

    21834

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_view,name='home'),
-    path('thank-you/',views.thank_you_view,name='thank_you'),
+    path('',views.homepage,name='homepage'),
     
 ]
