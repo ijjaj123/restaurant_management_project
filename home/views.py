6c0eab06a35ba22caa422422018130e267d1fb06
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-07 18:25:44 +0530

    21836

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
-from .forms import ContactFrom
+from .forms import ContactForm
 import requests
-form .models import M
+form .models import MenuItem
 
-
+def menu_page(request):
+    menu_items=MenuItem.objects.all()
+    return render(request,'home/menu.html',{'menu_items':menu_items})
 
 def homepage(request):
     response=requests.get('http://localhost:8000/api/menu-items/')
