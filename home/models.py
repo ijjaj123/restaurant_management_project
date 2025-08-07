cee90508f9997304d00a3364d6527bd5713dbf6f
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-07 14:59:23 +0530

    21835

--- home/models.py
+++ home/models.py
@@ -7,3 +7,8 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price
