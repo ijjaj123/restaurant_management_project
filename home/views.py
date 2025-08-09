aa15a61c33f0c37ad5c6eed7b0f449dcbe740e1c
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-09 12:58:04 +0530

    25715

--- home/views.py
+++ home/views.py
@@ -5,7 +5,30 @@ from .models import MenuItem
 from django.conf import settings
 
 def homepage(request):
-    rest
+    restuarant_name=settings.RESTAURANT_NAME 
+
+    menu_items=[]
+    try:
+        response=request.get('http://localhost:8000/api/menu-items/')
+        if response.status_code==200:
+            menu_items=response.json()
+        else:
+            menu_items=MenuItem.objects.all()
+    except:
+        menu_items=MenuItem.objects.all()
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+        else:
+            form=ContactForm()
+        
+        return render(request,'home/homepage.html',{
+            'restaurant_name':restaurant_name,
+            'menu_items':menu_items,
+            'form':form,
+        })
 
 
 # Create your views here.
