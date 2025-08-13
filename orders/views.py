ecdb8cad586419bea5d72bad95d5807883a4f563
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-13 14:21:30 +0530

    31908

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def menu_list(request):
+    menu_items=[
+        {"name":"Margerita pizza","price":8.99,"description":"classic pizza with fresh tomatoes,mozzaraella,and basil."}
+        {"name": "veg Burger","price":5.49,"description":"A delicious vegetable patty with lettuce and tomato."},
+        {"name":"pasta Alfredo","price":7.99,"description":"creamy Alfredo pasta with mushrooms and parmesan."},
+    ]
