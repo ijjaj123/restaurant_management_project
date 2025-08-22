5380ec41ca50c228cb95be3cb85d12c9b3ba6103
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-22 10:54:11 +0530

    36423

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.timezone import now
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "home.context_processors.restaurant_info",
             ],
         },
     },
