58190f20103104b69f1dc7b414e1dc7e99033810
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-24 14:32:14 +0530

    37573

--- home/models.py
+++ home/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class RestaurantLocation(models.Model):
+    address=models.CharField(max_length=255)
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    zip_code=models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.address},{self.city},{self.state} {self.zip_code}"
 class RestaurantInfo(models.Model):
     name=models.CharField(max_length=100)
     phone=models.CharField(max_length=15)
