ed3d58656bfc40cb0119eadb955bd24b888adea4
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-24 12:23:19 +0530

    37570

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact,MenuItem,RestaurantInfo,Feedback
 
 # Register your models here.
 admin.site.register(Contact)
+@admin,register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display=("name","price","image")
