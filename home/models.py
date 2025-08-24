32b9dc6913277727153d1e199f7466e4e5a1ca98
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-24 14:20:18 +0530

    37572

--- home/models.py
+++ home/models.py
@@ -28,3 +28,10 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback{self.id}-{self.created_at.strftime('%Y-%m-%d %H:%M)}"
+
+class ContactSubmission(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    submitted_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.name}-{self.email}"
