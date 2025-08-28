e575678f88dc55c5488f6dc55ceb863f995b1c82
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-28 18:27:04 +0530

    39441

--- home/forms.py
+++ home/forms.py
@@ -4,4 +4,11 @@ from .models import Contact
 class ContactForm(froms.ModelForm):
     class Meta:
         model=Contact
-        fields=['name','email']
+        fields=['name','email',"subject","message"]
+        widgets={
+            "name":forms.TextInput(attrs={"class":"input","placheholder":"your name"}),
+            "email":forms.EmailInput(attrs={"class":"input","placheholder":"your@example.com"}),
+            "subject":forms.TextInput(attrs={"class":"input","placheholder":"subject(optional)"}),
+            "message":forms.Textarea(attrs={"class":"textarea","rows":5,"placheholder":"your message"}),
+            
+        }
