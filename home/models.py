7e0180a48fb57261f467be6a4cb436f31276c3b7
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-28 18:20:17 +0530

    39441

--- home/models.py
+++ home/models.py
@@ -19,6 +19,9 @@ class RestaurantInfo(models.Model):
 class Contact(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField()
+    subject=models.CharField(max_length=150, blank=True)
+    message=models.TextField()
+    submitted_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
