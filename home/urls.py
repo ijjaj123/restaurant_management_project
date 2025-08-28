d99a6561093fce1b3723b741d466d2274537f2af
ijjaj123 <ijaj1155ahmed@gmail.com>
2025-08-28 12:28:45 +0530

    39440

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.homepage,name='homepage'),
     path('menu/',views.menu_page,name="menu"),
     path('contact/',views.contact_us,name='contact'),
-    path('reservations/',views.reservations,name='reservations')  
+    path('reservations/',views.reservations,name='reservations'),
     path("feedback/",views.feedback_view,name="feedback"),  
     
 ]
